54d5b0c4ad14866da19477692af74f988b5e04ee
LLDay <ssdenis99@gmail.com>
2017-11-14 19:04:59 +0300

    SpamFilter

--- test/spamFilter/Tests.kt
+++ test/spamFilter/Tests.kt
@@ -70,11 +70,11 @@ class Tests {
         catch(e: IllegalArgumentException) { assert(true) }
         catch(e: Throwable) { assert(false) }
 
-        assertEquals( spamList("Nick 11:11"), listOf<String>())
-        assertEquals( spamList("Nick 3:05"), listOf<String>())
-        assertEquals( spamList("Nick 10:10\nNick 11:10"), listOf<String>())
-        assertEquals( spamList("Nick 10:10\nNick 10:11"), listOf("Nick"))
-        assertEquals( spamList("Nick1 10:10\nNick2 3:18\nNick1 10:11"), listOf("Nick1"))
-        assertEquals( spamList("Nick1 10:10\nNick2 3:18\nNick3 10:11\nNick2 3:17\nNick1 10:11").toSet(), setOf("Nick1", "Nick2"))
+        assertEquals(spamList("Nick 11:11"), listOf<String>())
+        assertEquals(spamList("Nick 3:05"), listOf<String>())
+        assertEquals(spamList("Nick 10:10\nNick 11:10"), listOf<String>())
+        assertEquals(spamList("Nick 10:10\nNick 10:11"), listOf("Nick"))
+        assertEquals(spamList("Nick1 10:10\nNick2 3:18\nNick1 10:11"), listOf("Nick1"))
+        assertEquals(spamList("Nick1 10:10\nNick2 3:18\nNick3 10:11\nNick2 3:17\nNick1 10:11").toSet(), setOf("Nick1", "Nick2"))
     }
 }
